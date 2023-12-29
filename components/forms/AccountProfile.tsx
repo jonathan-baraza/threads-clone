5fa3a8e6aee7acfe3c66d66a314679554e0ed0fb
jonathan_baraza <jbaraza46@gmail.com>
2023-12-29 11:36:23 +0300

    shadcn componenets

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,19 +1,70 @@
 "use client";
 import React from "react";
 
-interface Props{
-    user:{
-        id:string,
-        objecjtId:string,
-        username:string,
-        name:string,
-        bio:string,
-        image:string
-    },
-    btnTitle:string,
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userValidation } from "@/lib/validations/user";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import * as z from "zod";
+interface Props {
+  user: {
+    id: string;
+    objecjtId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
 }
-const AccountProfile = ({ user,btnTitle }: Props) => {
-  return <div>AccountProfile</div>;
+const AccountProfile = ({ user, btnTitle }: Props) => {
+  const form = useForm({
+    resolver: zodResolver(userValidation),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof userValidation>) => {
+    console.log(values);
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default AccountProfile;
